dba65a9b50caa63b842ec0811f6626a6a578b077
Michal Kubiak <michal@michkub.net>
2022-09-10 16:55:14 +0200

    JS: funkcje

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 const firstName = "Michał";
 const age = 37;
 
-console.log(firstName);
-console.log(age);
-
-console.log(
-  `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`
-);
-
-const heading = document.querySelector('.main__heading--js')
-
-console.log(heading.innerHTML);
+const heading = document.querySelector(".main__heading--js");
 
 heading.innerHTML = `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`;
 
-console.log(heading);
-
-const emptyParagraph = document.querySelector('.week-summary__description--js');
+const emptyParagraph = document.querySelector(".week-summary__description--js");
 
 emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem`;
+
+function greetOld(age, firstName) {
+  console.log(
+    `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`
+  );
+}
+
+const calculate = (myNumber) => myNumber * 7;
+
+const myResult = calculate(5);
+
+console.log(myResult);
+
+const greet = (age, firstName) => {
+  console.log(
+    `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat - funkcja strzałkowa`
+  );
+};
+
+greet(age, firstName);
+
+function createContent(querySelectorContent, content) {
+  const element = document.querySelector(querySelectorContent);
+  element.innerHTML = content;
+}
+
+createContent(".week-summary__description--js", "Witaj swiecie!");
+
+createContent(".week-summary__title--js", "Homework done!");
