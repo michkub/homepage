160fc7076e3633051fee27f1f8d65e8b801947a6
Michal Kubiak <michal@michkub.net>
2022-09-10 15:52:24 +0200

    homework week 4

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ console.log(
   `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`
 );
 
-const heading = document.querySelector('.main-heading--js')
+const heading = document.querySelector('.main__heading--js')
 
 console.log(heading.innerHTML);
 
 heading.innerHTML = `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`;
 
 console.log(heading);
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+
+emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem`;
