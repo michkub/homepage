66c952372ecd06d037041dc3f1024da794d53b85
Michal Kubiak <michal@michkub.net>
2022-09-10 15:24:19 +0200

    DOM -js join with css

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ console.log(age);
 console.log(
   `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`
 );
+
+const heading = document.querySelector('.main-heading--js')
+
+console.log(heading.innerHTML);
+
+heading.innerHTML = `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`;
+
+console.log(heading);
