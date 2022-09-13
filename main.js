220d4b124a9a27a7046bd1229ed5233eb7055834
Michal Kubiak <michal@michkub.net>
2022-09-13 13:32:10 +0200

    Tabele i listy

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const heading = document.querySelector(".main__heading--js");
 
 heading.innerHTML = `Witaj Drogi Odwiedzający. Nazywam się ${firstName} i mam ${age} lat!`;
 
-const emptyParagraph = document.querySelector(".week-summary__description--js");
+//const emptyParagraph = document.querySelector(".week-summary__description--js");
 
-emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem`;
+//emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem`;
 
 function greetOld(age, firstName) {
   console.log(
@@ -37,3 +37,40 @@ function createContent(querySelectorContent, content) {
 createContent(".week-summary__description--js", "Witaj swiecie!");
 
 createContent(".week-summary__title--js", "Homework done!");
+
+
+const deathStar = {
+  diameter: 120000,
+  fire: (target) => {
+    console.log(`${target} destroyed `)
+  },
+  isOperating: true,
+  levels:357,
+  name: 'Death Star',
+  population: 10000,
+  isLightOn: true,
+  commander: {
+    name: 'Darth Vader',
+    age: 44,
+  }
+}
+
+
+deathStar.fire('Rebel ship')
+
+console.log(deathStar['name'])
+
+
+const humanOne = {
+  name: 'Maciek',
+  age: 32,
+}
+
+const humanTwo = {
+  name: 'Stefan',
+  age: 37,
+}
+
+humanTwo.age = 35;
+
+console.log(humanTwo.age);
